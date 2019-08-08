278f8eb72c12f1ef322727cc74acfc3e2222b472
LDK <lidengke0221@126.com>
2019-08-08 22:32:43 +0800

    组件封装

--- components/Tabs/Tab.vue
+++ components/Tabs/Tab.vue
@@ -1,20 +1,127 @@
 <template>
-  <div class="tab-warpper">
-    <div class="tab-title">{{ name }}</div>
+  <div class="app-container">
+    <div class="tabs-warpper">
+      <section class="tabs-left">
+        <ul class="tabs-title-warpper">
+          <li
+            class="tabs-title"
+            v-for="(item, index) in tabs"
+            :key="index"
+            :class="[index === currentIndex ? 'tabs-title-active' : '', className ]"
+            @click="tabsHandle(index)">
+            {{ item }}
+          </li>
+        </ul>
+      </section>
+      <section ref="content" class="tabs-right">
+        <div class="tabs-content-warpper">
+          <div class="tabs-content">
+            <slot></slot>
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'ITabs',
   props: {
-    name: {
-      type: String,
-      default: 'Tab'
+    tabs: {
+      type: Array,
+      default: function() {
+        return []
+      }
+    },
+    className: String,
+    width: {
+      type: [String, Number],
+      default: '100px'
+    }
+  },
+  data() {
+    return {
+      currentIndex: 0
+    }
+  },
+  computed: {},
+  methods: {
+    tabsHandle(index) {
+      this.currentIndex = index
+      this.$emit('on-click', index)
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.app-container {
+  padding: 15px;
+}
+.tabs-warpper {
+  font-size: 0;
+  border: 1px solid #eaeaea;
+  .tabs-left {
+    width: 100px;
+    display: inline-block;
+    vertical-align: top;
+    font-size: 14px;
+    overflow: hidden;
+    .tabs-title-warpper {
+      height: 200px;
+      overflow-x: hidden;
+      width: calc(100% + 17px);
+      font-size: 16px;
+      overflow-y: auto;
+      border-right: 1px solid #eaeaea;
+      .tabs-title {
+        text-align: center;
+        padding: 10px 10px;
+        &:hover {
+          background-color: #f8f8f8;
+          cursor: pointer;
+        }
+        &.tabs-title-active {
+          color: #FFF;
+          background-color: #52ccba;
+        }
+      }
+    }
+  }
+  .tabs-right {
+    width: calc(100% - 101px);
+    height: 200px;
+    display: inline-block;
+    vertical-align: top;
+    font-size: 14px;
+    overflow: hidden;
+    .tabs-content-warpper {
+      width: calc(100% + 17px);
+      height: 200px;
+      padding: 20px;
+      overflow-x: hidden;
+      overflow-y: auto;
+      .tabs-content {
+        font-size: 14px;
+        .user-name {
+          width: 100px;
+          padding: 5px 10px;
+          margin-bottom: 15px;
+          margin-right: 15px;
+          display: inline-block;
+          display: block;
+          text-align: center;
+          color: #404040;
+          border: 1px solid #eaeeae;
+          &:hover {
+            color: #fff;
+            background-color: #52ccba;
+            cursor: pointer;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
--- pages/tabs/index.vue
+++ pages/tabs/index.vue
@@ -1,75 +1,46 @@
 <template>
   <div class="app-container">
-    <div class="tabs-warpper">
-      <section class="tabs-left">
-        <ul class="tabs-title-warpper">
-          <li class="tabs-title" v-for="item in tabs" :key="item.id" :class="{'tabs-title-active': item.id === currentIndex}" @click="tabsHandle(item.id)">
-            <!-- <component :is="componentId"></component> -->
-            {{ item.title }}
-          </li>
-          <!-- <li class="tabs-title">
-            Tabs2
-          </li>
-          <li class="tabs-title">
-            Tabs3
-          </li>
-          <li class="tabs-title">
-            Tabs4
-          </li>
-          <li class="tabs-title">
-            Tabs5
-          </li>
-          <li class="tabs-title">
-            Tabs6
-          </li> -->
-        </ul>
-      </section>
-      <section class="tabs-right">
-        <ul class="tabs-content-warpper">
-          <li class="tabs-content">
-            <a href="javascript:void(0);" class="user-name" v-for="item in list" :key="item.id">{{ item.name }}</a>
-          </li>
-          <!-- <li class="tabs-content">
-            Content 1
-          </li>
-          <li class="tabs-content">
-            Content 2
-          </li>
-          <li class="tabs-content">
-            Content 3
-          </li>
-          <li class="tabs-content">
-            Content 4
-          </li>
-          <li class="tabs-content">
-            Content 5
-          </li>
-          <li class="tabs-content">
-            Content 6
-          </li> -->
-        </ul>
-      </section>
-    </div>
+    <i-tabs :tabs="tabs" :width="100" @on-click="tabsClick">
+      <p v-for="(item, index) in list" :key="index">{{ item.name }}</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+      <p>这是一个测试</p>
+    </i-tabs>
   </div>
 </template>
 
 <script>
+import ITabs from '@/components/Tabs/Tab'
 export default {
+  components: { ITabs },
   asyncData({isDev, route, store, env, params, query, req, res, redirect, error}) {
     // 
   },
   data() {
     return {
-      componentId: 1,
-      currentIndex: 1,
-      tabs: [
-        { id: 1, title: 'Tabs 1' },
-        { id: 2, title: 'Tabs 2' },
-        { id: 3, title: 'Tabs 3' },
-        { id: 4, title: 'Tabs 4' },
-        { id: 5, title: 'Tabs 5' },
-        { id: 6, title: 'Tabs 6' }
-      ],
+      tabs: ['Tabs 1', 'Tabs 2', 'Tabs 3', 'Tabs 4', 'Tabs 5', 'Tabs 6', 'Tabs 7'],
       list: [
         { id: 1, name: '张三' },
         { id: 2, name: '李四' },
@@ -86,8 +57,7 @@ export default {
     }
   },
   methods: {
-    tabsHandle(value) {
-      this.currentIndex = value
+    tabsClick(value) {
       this.list = this.list.reverse()
     }
   }
@@ -95,72 +65,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.app-container {
-  padding: 15px;
-}
-.tabs-warpper {
-  font-size: 0;
-  border: 1px solid #eaeaea;
-  .tabs-left {
-    width: 100px;
-    display: inline-block;
-    vertical-align: top;
-    font-size: 14px;
-    overflow: hidden;
-    .tabs-title-warpper {
-      height: 200px;
-      overflow-x: hidden;
-      width: 117px;
-      font-size: 16px;
-      overflow-y: auto;
-      border-right: 1px solid #eaeaea;
-      .tabs-title {
-        text-align: center;
-        padding: 10px 10px;
-        &:hover {
-          background-color: #f8f8f8;
-          cursor: pointer;
-        }
-        &.tabs-title-active {
-          color: #FFF;
-          background-color: #52ccba;
-        }
-      }
-    }
-  }
-  .tabs-right {
-    width: calc(100% - 101px);
-    height: 200px;
-    display: inline-block;
-    vertical-align: top;
-    font-size: 14px;
-    overflow: hidden;
-    .tabs-content-warpper {
-      width: calc(100% + 17px);
-      height: 200px;
-      padding: 20px;
-      overflow-x: hidden;
-      overflow-y: auto;
-      .tabs-content {
-        font-size: 14px;
-        .user-name {
-          width: 100px;
-          padding: 5px 10px;
-          margin-bottom: 15px;
-          margin-right: 15px;
-          display: inline-block;
-          display: block;
-          text-align: center;
-          color: #404040;
-          border: 1px solid #eaeeae;
-          &:hover {
-            color: #fff;
-            background-color: #52ccba;
-            cursor: pointer;
-          }
-        }
-      }
-    }
-  }
-}
+
 </style>
