5c80929c340b472cc0f38a21c044b808957b42dd
lidengke <lidengke@mh-tech.cn>
2019-08-13 10:11:00 +0800

    登录霓虹灯效果

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -2,7 +2,13 @@
   <!-- <section class="login" :class="loginbgClass"> -->
   <section class="login">
     <div class="form-warpper">
-      <h2 class="title">登录</h2>
+      <!-- <h2 class="title">登录</h2> -->
+      <svg class="title" width="100%" height="100">
+        <text text-anchor="middle" x="50%" y="50%" class="text text-1">登录</text>
+        <text text-anchor="middle" x="50%" y="50%" class="text text-2">登录</text>
+        <text text-anchor="middle" x="50%" y="50%" class="text text-3">登录</text>
+        <text text-anchor="middle" x="50%" y="50%" class="text text-4">登录</text>
+      </svg>
       <form @submit.prevent>
         <div class="form-item">
           <label for="" class="name">用户名：</label>
@@ -165,8 +171,6 @@ export default {
   box-shadow: 2px 2px 10px #f8f8f8;
   .title {
     text-align: center;
-    margin-top: 20px;
-    margin-bottom: 40px;
     font-size: 24px;
     color: #FFF;
   }
@@ -257,5 +261,42 @@ export default {
 }
 .login-leave {
   opacity: 0;
+}
+</style>
+<style lang="scss" scoped>
+.text {
+  font-size: 64px;
+  font-weight: bold;
+  text-transform: uppercase;
+  fill: none;
+  stroke-width: 2px;
+  stroke-dasharray: 90 310;
+  animation: stroke 6s infinite linear;
+}
+.text-1 {
+  stroke: #3498db;
+  text-shadow: 0 0 5px #3498db;
+  animation-delay: -1.5s;
+}
+.text-2 {
+  stroke: #f39c12;
+  text-shadow: 0 0 5px #f39c12;
+  animation-delay: -3s;
+}
+.text-3 {
+  stroke: #e74c3c;
+  text-shadow: 0 0 5px #e74c3c;
+  animation-delay: -4.5s;
+}
+.text-4 {
+  stroke: #9b59b6;
+  text-shadow: 0 0 5px #9b59b6;
+  animation-delay: -6s;
+}
+
+@keyframes stroke {
+  100% {
+    stroke-dashoffset: -400;
+  }
 }
 </style>
