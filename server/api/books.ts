d01a1eca0158dc9bb37722626f51c176ee21ad50
ldk <lidengke0221@126.com>
2024-11-19 11:07:07 +0800

    update

--- server/api/books.ts
+++ server/api/books.ts
@@ -1,10 +1,18 @@
 
 export default defineEventHandler(async event => {
   const { keyword } = getQuery(event)
-  const data = await fetch(`https://book-db-v1.saltyleo.com/?keyword=${keyword}`).then(res => res.json())
-  return {
-    code: 200,
-    data: data,
-    message:'success'
+  try {
+    const data = await fetch(`https://book-db-v1.saltyleo.com/?keyword=${keyword}`).then(res => res.json())
+    return {
+      code: 200,
+      data: data,
+      message:'success'
+    }
+  } catch (err) {
+    return {
+      code: 200,
+      data: [],
+      message: err
+    }
   }
 })
